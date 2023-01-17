f6c14dbeaa107a9d397007ffff9cf3bf5664fa29
InoKotaro <ino.kota0602@gmail.com>
2023-01-17 11:32:36 +0000

    [add]いいねボタン非同期化完了

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,10 @@
 class HomesController < ApplicationController
 
   def top
+    p "=========================================="
+    p params[:page]
     @posts = Post.all
-    @posts_page = Post.page(params[:page]).per(3) #ページネーション
+    @posts_page = Post.page(params[:page]) #ページネーション
   end
 
   def about
